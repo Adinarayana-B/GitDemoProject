570fa02d2fd6516024b28ce1d70a23f9fb0a63e7
91970 <91970@LAPTOP-JM8RN7EK>
2022-05-25 21:08:27 +0530

    First commit....

---
+++ src/repositoryDemo/Test1.java
@@ -0,0 +1,18 @@
+package repositoryDemo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String fruit="banana";
+		if(fruit=="banana")
+		{
+			System.out.println("banana block");
+			
+		}
+		else
+		{
+			System.out.println("no banana no energy");
+		}
+	}
+
+}
